019f25e930710ed3c21b2b298ae01ed89a448bab
brunchking <francis545@yahoo.com.tw>
2019-08-15 14:44:19 +0800

    0815

--- routes/weather.js
+++ routes/weather.js
@@ -14,7 +14,6 @@ let isLogin = false;
 let first = false;
 
 router.get('/', async (req, res, next) => {
-    viewbag = {};
     let api_key = process.env.API_KEY;
     let api_url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${api_key}&format=JSON&elementName=MinT,MaxT`;
     const url1 = 'https://www.cwb.gov.tw/Data/js/Observe/Observe_Home.js?';
@@ -22,40 +21,56 @@ router.get('/', async (req, res, next) => {
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     let weatherData = [];
-    
+
     if (req.isAuthenticated()) {
         isLogin = true;
     }
 
-    if (req.user === undefined) {
-        isLogin = false;
-    }
+    
 
-    if (first === true) {
-        // Downloading the observe data into local file
-        const Observe_Home = fs.createWriteStream(__dirname + "./../public/js/weather/current_weather/Observe_Home.js");
-        const request = https.get(url1, function (response) {
-            response.pipe(Observe_Home);
-        });
-
-
-        // Downloading the W50 data into local file
-        const W50_Data = fs.createWriteStream(__dirname + "./../public/js/weather/W50_Data.js");
-        const request2 = https.get(url2, function (response) {
-            response.pipe(W50_Data);
-        });
-        first = true;
-    }
-
-
-    // Render the page. const weatherData = await weatherInfo(json, city.twentyOne);
-    Object.keys(city).forEach(async (data, index) => {
-        weatherData[index] = weatherInfo(json, city[data]);
-        if (index === 21) {
-            res.render('weather', { layout: false, isLogin: isLogin, weatherData });
-            // weatherData is an array
-        }
+    fetch(url1).then(response => response.text()).then(data => {
+        fs.writeFileSync(__dirname + "./../public/js/weather/current_weather/Observe_Home.js", data)
+    }).then(() => {
+        fetch(url2).then(response => response.text()).then(data => {
+            fs.writeFileSync(__dirname + "./../public/js/weather/W50_Data.js", data);
+        }).then(() => {
+            Object.keys(city).forEach(async (data, index) => {
+                weatherData[index] = weatherInfo(json, city[data]);
+                if (index === 21) {
+                    res.render('weather', { layout: false, isLogin: isLogin, weatherData });
+                    // weatherData is an array
+                }
+            })
+        })
     })
+
+
+
+    // if (first === false) {
+    //     // Downloading the observe data into local file
+    //     const Observe_Home = fs.createWriteStream(__dirname + "./../public/js/weather/current_weather/Observe_Home.js");
+    //     const request = https.get(url1, function (response) {
+    //         response.pipe(Observe_Home);
+    //     });
+
+
+    //     // Downloading the W50 data into local file
+    //     const W50_Data = fs.createWriteStream(__dirname + "./../public/js/weather/W50_Data.js");
+    //     const request2 = https.get(url2, function (response) {
+    //         response.pipe(W50_Data);
+    //     });
+    //     first = true;
+    // }
+
+
+    // // Render the page. const weatherData = await weatherInfo(json, city.twentyOne);
+    // Object.keys(city).forEach(async (data, index) => {
+    //     weatherData[index] = weatherInfo(json, city[data]);
+    //     if (index === 21) {
+    //         res.render('weather', { layout: false, isLogin: isLogin, weatherData });
+    //         // weatherData is an array
+    //     }
+    // })
 });
 
 module.exports = router;
