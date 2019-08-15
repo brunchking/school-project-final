7e7ce74ceb46b69303b333442aa5254a2d21c1dd
brunchking <francis545@yahoo.com.tw>
2019-08-15 16:20:57 +0800

    cool

--- public/js/weather/current_weather/Observe_Home.js
+++ public/js/weather/current_weather/Observe_Home.js
@@ -1,32 +1,32 @@
-var ObserveTime = '08/15 14:50';
+var ObserveTime = '08/15 16:10';
 var OBS = {
-        0:{'CountyID':'63','CountyName':{'C':'臺北','E':'Taipei City'},'Temperature':{'C':'33.8','F':'93'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'51','Rain':'0.0'
+        0:{'CountyID':'63','CountyName':{'C':'臺北','E':'Taipei City'},'Temperature':{'C':'33.0','F':'91'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'53','Rain':'0.0'
     },
-        1:{'CountyID':'10004','CountyName':{'C':'新竹','E':'Hsinchu County'},'Temperature':{'C':'32.7','F':'91'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'59','Rain':'0.0'
+        1:{'CountyID':'10004','CountyName':{'C':'新竹','E':'Hsinchu County'},'Temperature':{'C':'32.6','F':'91'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'60','Rain':'0.0'
     },
-        2:{'CountyID':'66','CountyName':{'C':'臺中','E':'Taichung City'},'Temperature':{'C':'30.1','F':'86'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'84','Rain':'65.0'
+        2:{'CountyID':'66','CountyName':{'C':'臺中','E':'Taichung City'},'Temperature':{'C':'29.8','F':'86'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'78','Rain':'65.0'
     },
-        3:{'CountyID':'10010','CountyName':{'C':'嘉義','E':'Chiayi County'},'Temperature':{'C':'31.3','F':'88'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'73','Rain':'81.0'
+        3:{'CountyID':'10010','CountyName':{'C':'嘉義','E':'Chiayi County'},'Temperature':{'C':'28.2','F':'83'},'Weather_Icon':'18','Weather':{'C':'陰有陣雨','E':'CLOUDY WITH WITH SHOWER'},'Time':'day','Humidity':'85','Rain':'84.5'
     },
-        4:{'CountyID':'67','CountyName':{'C':'臺南','E':'Tainan City'},'Temperature':{'C':'30.9','F':'88'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'74','Rain':'1.0'
+        4:{'CountyID':'67','CountyName':{'C':'臺南','E':'Tainan City'},'Temperature':{'C':'30.2','F':'86'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'77','Rain':'1.0'
     },
-        5:{'CountyID':'64','CountyName':{'C':'高雄','E':'Kaohsiung City'},'Temperature':{'C':'30.3','F':'87'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'83','Rain':'7.0'
+        5:{'CountyID':'64','CountyName':{'C':'高雄','E':'Kaohsiung City'},'Temperature':{'C':'29.9','F':'86'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'83','Rain':'7.0'
     },
         6:{'CountyID':'10014','CountyName':{'C':'臺東','E':'Taitung County'},'Temperature':{'C':'-','F':'-'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'-','Rain':'1.0'
     },
         7:{'CountyID':'10015','CountyName':{'C':'花蓮','E':'Hualien County'},'Temperature':{'C':'-','F':'-'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'-','Rain':'-'
     },
-        8:{'CountyID':'10002','CountyName':{'C':'宜蘭','E':'Yilan County'},'Temperature':{'C':'29.6','F':'85'},'Weather_Icon':'14','Weather':{'C':'陰有雨','E':'CLOUDY WITH RAIN'},'Time':'day','Humidity':'85','Rain':'0.5'
+        8:{'CountyID':'10002','CountyName':{'C':'宜蘭','E':'Yilan County'},'Temperature':{'C':'30.9','F':'88'},'Weather_Icon':'34','Weather':{'C':'陰有雷聲','E':'CLOUDY WITH THUNDER'},'Time':'day','Humidity':'82','Rain':'0.5'
     },
         9:{'CountyID':'10016','CountyName':{'C':'澎湖','E':'Penghu County'},'Temperature':{'C':'29.7','F':'85'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'-','Rain':'26.5'
     },
-        10:{'CountyID':'09020','CountyName':{'C':'金門','E':'Kinmen County'},'Temperature':{'C':'33.9','F':'93'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'69','Rain':'0.0'
+        10:{'CountyID':'09020','CountyName':{'C':'金門','E':'Kinmen County'},'Temperature':{'C':'32.3','F':'90'},'Weather_Icon':'99','Weather':{'C':'X','E':'X'},'Time':'day','Humidity':'74','Rain':'0.0'
     },
-        11:{'CountyID':'09007','CountyName':{'C':'馬祖','E':'Lienchiang County'},'Temperature':{'C':'30.7','F':'87'},'Weather_Icon':'24','Weather':{'C':'晴有靄','E':'CLEAR WITH MIST'},'Time':'day','Humidity':'79','Rain':'0.0'
+        11:{'CountyID':'09007','CountyName':{'C':'馬祖','E':'Lienchiang County'},'Temperature':{'C':'29.2','F':'85'},'Weather_Icon':'99','Weather':{'C':'X','E':'X'},'Time':'day','Humidity':'78','Rain':'0.0'
     },
-        12:{'CountyID':'10013','CountyName':{'C':'恆春','E':'Pingtung County'},'Temperature':{'C':'27.4','F':'81'},'Weather_Icon':'18','Weather':{'C':'陰有陣雨','E':'CLOUDY WITH WITH SHOWER'},'Time':'day','Humidity':'99','Rain':'7.5'
+        12:{'CountyID':'10013','CountyName':{'C':'恆春','E':'Pingtung County'},'Temperature':{'C':'28.4','F':'83'},'Weather_Icon':'07','Weather':{'C':'陰','E':'CLOUDY'},'Time':'day','Humidity':'97','Rain':'7.5'
     },
-        13:{'CountyID':'64001','CountyName':{'C':'東沙','E':'Pratas'},'Temperature':{'C':'27.8','F':'82'},'Weather_Icon':'08','Weather':{'C':'多雲有雨','E':'PARTLY CLOUDY WITH RAIN'},'Time':'day','Humidity':'90','Rain':'22.5'
+        13:{'CountyID':'64001','CountyName':{'C':'東沙','E':'Pratas'},'Temperature':{'C':'28.5','F':'83'},'Weather_Icon':'04','Weather':{'C':'多雲','E':'PARTLY CLOUDY'},'Time':'day','Humidity':'89','Rain':'22.5'
     },
         14:{'CountyID':'64002','CountyName':{'C':'南沙','E':'Nansha'},'Temperature':{'C':'-','F':'-'},'Weather_Icon':'01','Weather':{'C':'晴','E':'CLEAR'},'Time':'day','Humidity':'-','Rain':'-'
     }
