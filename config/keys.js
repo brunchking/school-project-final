abb09d3b34b9fc26bb5a2998b161c975955f51a8
brunchking <44939916+brunchking@users.noreply.github.com>
2021-12-19 14:36:06 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    mongoURI: 'mongodb+srv://francis545:castle123@schoolproject-rsfbq.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://francis545:密碼@schoolproject-rsfbq.mongodb.net/test?retryWrites=true&w=majority'
 };
